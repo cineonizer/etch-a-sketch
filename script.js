ec928a12d4ce2d018da646e55dcbd3cb6dc63a0e
cineonizer <jamesdkiam@gmail.com>
2021-01-13 21:28:22 -0800

    almost finished

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
 const mainContainer = document.querySelector('.main-container');
 
-function createGrid(size) {
-    for (let i = 0; i < size ** 2; i++) {
-        let div = document.createElement('div');
-        mainContainer.appendChild(div).classList = 'cell';
-        mainContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-        mainContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+function resetGrid() {
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+}
+
+function toggleGridlines() {
+    const allDivs = [...mainContainer.children];
+    allDivs.forEach(allDivs => {
+        allDivs.classList.toggle('cell');
+    });
+}
+
+function changeGridText() {
+    const gridSizeValue = document.querySelector('#grid-slider').value;
+    const gridSizeText = document.querySelector('#grid-text');
+    gridSizeText.textContent = gridSizeValue;
+    gridSizeText.addEventListener('mouseenter', () => {
+        gridSizeText.textContent = "Size";
+    });
+    gridSizeText.addEventListener('mouseleave', () => {
+        gridSizeText.textContent = gridSizeValue;
+    });
+    gridSizeText.addEventListener('click', () => {
+        gridSizeText.textContent = "Size";
+    });
+    return gridSizeValue;
+}
+
+function createGrid() {
+    changeGridText();
+    const gridSizeValue = document.querySelector('#grid-slider').value;
+    for (let i = 0; i < gridSizeValue ** 2; i++) {
+        let cellDiv = document.createElement('div');
+        mainContainer.appendChild(cellDiv).classList.add('cell', 'cell-shadow');
+        mainContainer.style.gridTemplateRows = `repeat(${gridSizeValue}, 1fr)`;
+        mainContainer.style.gridTemplateColumns = `repeat(${gridSizeValue}, 1fr)`;
     }
 }
 
 function changeCellColor() {
+    const colorButton = document.querySelector('#color-button');
+    let cellColor = colorButton.value;
+    colorButton.addEventListener('input', () => {
+        cellColor = colorButton.value;
+    });
+
     let mouseDown = false;
+    let hoverState = true;
     const cells = document.querySelectorAll('.cell');
+
     cells.forEach(cells => {
         cells.addEventListener('mousedown', () => {
-            cells.style.backgroundColor = "orange";
+            cells.style.backgroundColor = cellColor;
             mouseDown = true;
         });
-    });
-    cells.forEach(cells => {
+
         cells.addEventListener('mousemove', () => {
             if (mouseDown === true) {
-                cells.style.backgroundColor = "orange";
+                cells.style.backgroundColor = cellColor;
             }
         });
+
     });
     window.addEventListener('mouseup', () => {
         mouseDown = false;
@@ -33,14 +72,15 @@ function changeCellColor() {
 function clearGrid() {
     const clear = document.querySelector('#clear-button');
     const allDivs = [...mainContainer.children];
-    clear.addEventListener('click', function() {
+    clear.addEventListener('click', () => {
         allDivs.forEach(allDivs => {
             allDivs.style.backgroundColor = '';
         });
     });
 }
 
-
-createGrid(16);
+resetGrid();
+changeGridText();
+createGrid();
 changeCellColor();
 clearGrid();
